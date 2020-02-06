ac9aa624d77ad0d7bd62d7a7a1275076dc3e25b5
Capitaine Gloomy = Nicolas Chambon <n.chambon84@gmail.com>
2020-02-06 15:46:18 +0100

    Ajout dossier Data, Ajout MagasinDbContext Ajout Model.cs, il faut faire un model par entité

---
+++ StockManagement/Data/MagasinDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TPBibliotheque
+{
+    public class MagasinDbContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // création de la classe de configuration pour l'entité "Student"
+            var articleEntity = modelBuilder.Entity<Article>();
+            // définition du mapping
+            articleEntity.HasKey(a => a.Id); // définition du champ clé
+
+            // création de la classe de configuration pour l'entité "Teacher"
+            var etagereEntity = modelBuilder.Entity<Etagere>();
+            // définition du mapping
+            etagereEntity.HasKey(e => e.Id);
+           
+            //création des relations
+
+            etagereEntity
+                .HasOne(s => s.Secteur)
+                .WithMany(e => e.Etageres)
+                .HasForeignKey(s => s.SecteurId);
+
+            // création de la classe de configuration pour l'entité "ApprenticeLink"
+            var positionMagasin = modelBuilder.Entity<PositionMagasin>();
+            //définition du mapping n n
+            positionMagasin.HasKey(pm => new { pm.EtagereId, pm.ArticleId }); // définition d'une clé composée
+            positionMagasin
+                .HasOne(pm => pm.Etagere)
+                .WithMany(e => e.PositionMagasin)
+                .HasForeignKey(pm => pm.EtagereId);
+            positionMagasin
+                .HasOne(pm => pm.Article)
+                .WithMany(a => a.PositionMagasin)
+                .HasForeignKey(pm => pm.ArticleId);
+
+            // Secteur 
+            var secteurEntity = modelBuilder.Entity<Secteur>();
+                secteurEntity.HasKey(s => s.Id);
+                secteurEntity.Property(s => s.Name).HasMaxLength(256).IsRequired();
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // définition de la base de données à utiliser ainsi que de la chaine de connexion
+
+            optionsBuilder.UseSqlite("Filename=test.db");
+            /*SqlLite ou SqlServer (direct sur vs) */
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
+
+
---
+++ StockManagement/Models/Models.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPBibliotheque
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string libelle { get; set; }
+        public string SKU { get; set; }
+        public DateTime DateSortie { get; set; }
+        public decimal PrixInitial { get; set; }
+        public decimal Poids { get; set; }
+        public ICollection<PositionMagasin> PositionMagasin { get; set; }
+    }
+
+    public class PositionMagasin
+    {
+        public int ArticleId { get; set; }
+        public Article Article { get; set; }
+        public int EtagereId { get; set; }
+        public Etagere Etagere { get; set; }
+        public int Quantite { get; set; }
+    }
+
+    public class Etagere
+    {
+        public int Id { get; set; }
+        public decimal PoidMaximum { get; set; }
+        public int SecteurId { get; set; }
+        public Secteur Secteur { get; set; }
+        public ICollection<PositionMagasin> PositionMagasin { get; set; }
+    }
+
+    public class Secteur
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Etagere> Etageres { get; set; }
+
+    }
+}
