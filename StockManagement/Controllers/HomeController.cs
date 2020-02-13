1771dd73e17ff1f517cc2ca1c81256c5c430a1f2
Sid Djilali Saiah <sid.djilali-saiah@epitech.eu>
2020-02-13 11:19:22 +0100

    changements

--- StockManagement/Controllers/HomeController.cs
+++ StockManagement/Controllers/HomeController.cs
@@ -23,13 +23,6 @@ namespace StockManagement.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- StockManagement/Services/Abstractions/IArticleRepository.cs
+++ StockManagement/Services/Abstractions/IArticleRepository.cs
@@ -12,11 +12,12 @@ namespace StockManagement.Services
         void Update(Article article);
         void Remove(Article article);
         Task Save();
-        public bool Exists(int id);
+        Task<Article> FindById(int articleId);
         Task<IEnumerable<Article>> GetAll();
+
+        public bool Exists(int id);
         IEnumerable<Article> GetAllByEtagere(int etagereId);
         IEnumerable<Article> GetAllBySecteur(int secteurId);
-        Task<Article> FindById(int articleId);
         decimal GetAveragePriceBySecteur(int secteurId);
     }
 }
