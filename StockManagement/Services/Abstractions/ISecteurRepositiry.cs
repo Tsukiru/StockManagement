0ea69868f690239d9a425f917bfa33f73847612e
Capitaine Gloomy = Nicolas Chambon <n.chambon84@gmail.com>
2020-02-13 14:15:13 +0100

    Ajout SecteurEFRepository et sont interface

---
+++ StockManagement/Services/Abstractions/ISecteurRepositiry.cs
@@ -0,0 +1,22 @@
+﻿using StockManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockManagement.Services
+{
+    interface ISecteurRepositiry
+    {
+
+        void Insert(Secteur secteur);
+        void Update(Secteur secteur);
+        void Remove(Secteur secteur);
+        Task Save();
+        Task<Secteur> FindById(int secteurId);
+        Task<IEnumerable<Secteur>> GetAll();
+
+        public bool Exists(int id);
+
+    }
+}
---
+++ StockManagement/Services/Implementations/SecteurEFRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockManagement.Data;
+using StockManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockManagement.Services
+{
+    public class SecteurEFRepository : ISecteurRepositiry
+    {
+
+        private readonly MagasinDbContext context;
+
+        public SecteurEFRepository(MagasinDbContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Secteur> FindById(int id)
+        {
+            Secteur secteur = await context.Secteurs.FindAsync(id);
+            return secteur;
+        }
+
+        public async Task<IEnumerable<Secteur>> GetAll()
+        {
+            List<Secteur> secteurs = await context.Secteurs.ToListAsync();
+            return secteurs;
+        }
+
+        public bool Exists(int id)
+        {
+            return context.Secteurs.Any(e => e.Id == id);
+        }
+
+        public void Insert(Secteur secteur)
+        {
+            context.Secteurs.Add(secteur);
+        }
+
+        public void Remove(Secteur secteur)
+        {
+            context.Secteurs.Remove(secteur);
+        }
+
+        public void Update(Secteur secteur)
+        {
+            context.Secteurs.Update(secteur);
+        }
+
+        public async Task Save()
+        {
+            await context.SaveChangesAsync();
+        }
+
+    }
+}
