46676324ecf3c240af7dc3fba2f0498f8a198939
Capitaine Gloomy = Nicolas Chambon <n.chambon84@gmail.com>
2020-03-06 17:03:23 +0100

    fix bug libelle articles

--- StockManagement/Controllers/ArticlesController.cs
+++ StockManagement/Controllers/ArticlesController.cs
@@ -54,7 +54,7 @@ namespace StockManagement.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,libelle,SKU,DateSortie,PrixInitial,Poids")] Article article)
+        public async Task<IActionResult> Create([Bind("Id,Libelle,SKU,DateSortie,PrixInitial,Poids")] Article article)
         {
             if (ModelState.IsValid)
             {
